f0aa959cc501a0e1edd5b826b29d146515d0d0bc
KARTES25 <stilalex@yandex.ru>
2019-04-24 21:59:53 +0700

    removed input check

--- src/main.c
+++ src/main.c
@@ -9,17 +9,20 @@ int main() {
   int x1, y1;
   float r1;
   cout << "Enter parametr of 1st circle(WKT)" << endl;
-  cin >> x1 >> y1 >> r1;
-  if (r1 <= 0) {
-    cout << "Error:Circle can't have negative radius" << endl;
-  }
+  cin >> x1 >> y1 ;
+  do {
+    	cout<<"enter radius (more than 0)";
+    	cin>>r1;
+	} while (!is_more_then_0(r1));
   int x2, y2;
   float r2;
   cout << "Enter parametr of 2nd circle(WKT)" << endl;
-  cin >> x2 >> y2 >> r2;
-  if (r2 <= 0) {
-    cout << "Error:Circle can't have negative radius\n" << endl;
-  }
+  cin >> x2 >> y2 ;
+  
+do {
+    	cout<<"enter radius (more than 0)";
+    	cin>>r2;
+	} while (!is_more_then_0(r2));
   circle(x1, y1, r1);
   circle(x2, y2, r2);
 }
