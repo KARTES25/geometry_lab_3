43b90cd761ef3214b1637272ab8615b0b73c7db5
KARTES25 <stilalex@yandex.ru>
2019-05-05 21:34:57 +0700

    last change main

--- src/main.c
+++ src/main.c
@@ -1,29 +1,19 @@
-#include "is_more_then_0"
+#include "is_more_then_0.c"
 #include "circle.c"
-#include <iostream>
-#include <stdio.h>
-#include <stdlib.h>
-using namespace std;
 const float p = 3.1415912;
 
 int main() {
-  int x1, y1;
-  float r1;
-  cout << "Enter parametr of 1st circle(WKT)" << endl;
-  cin >> x1 >> y1 ;
-  do {
-    	cout<<"enter radius (more than 0)";
-    	cin>>r1;
-	} while (!is_more_then_0(r1));
-  int x2, y2;
-  float r2;
-  cout << "Enter parametr of 2nd circle(WKT)" << endl;
-  cin >> x2 >> y2 ;
-  
-do {
-    	cout<<"enter radius (more than 0)";
-    	cin>>r2;
-	} while (!is_more_then_0(r2));
-  circle(x1, y1, r1);
-  circle(x2, y2, r2);
+  	int x1, y1;
+  	float r1;
+  	cout << "Enter parametr of 1st circle(WKT)" << endl;
+  	cin >> x1 >> y1 ;
+
+       r1=is_more_then_0();
+  	int x2, y2;
+  	float r2;
+  	cout << "Enter parametr of 2nd circle(WKT)" << endl;
+  	cin >> x2 >> y2 ;
+  r2=is_more_then_0();
+  	circle(x1, y1, r1);
+  	circle(x2, y2, r2);
 }
