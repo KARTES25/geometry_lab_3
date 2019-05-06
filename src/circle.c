92711d8a0af06ed246ed67ba77bded541b839b96
KARTES25 <stilalex@yandex.ru>
2019-05-06 13:28:16 +0700

    clang-format for travis

--- src/circle.c
+++ src/circle.c
@@ -3,13 +3,13 @@
 #include <stdlib.h>
 using namespace std;
 
-int circle(int x1, int y1, float r1)
-{   float p=3.14159265359;
-    cout<<"circle1"<< x1<< y1<< r1;
-    float s1, p1;
-    p1 = 2 * p * r1;
-    s1 = p * r1 * r1;
-    cout<<"perimetr = "<< p1<< endl;
-    cout<<"area = "<< s1<<endl;
-    return 0;
+int circle(int x1, int y1, float r1) {
+  float p = 3.14159265359;
+  cout << "circle1" << x1 << y1 << r1;
+  float s1, p1;
+  p1 = 2 * p * r1;
+  s1 = p * r1 * r1;
+  cout << "perimetr = " << p1 << endl;
+  cout << "area = " << s1 << endl;
+  return 0;
 }
--- src/is_more_then_0.c
+++ src/is_more_then_0.c
@@ -3,16 +3,12 @@
 #include <stdlib.h>
 using namespace std;
 
-void is_more_then_0(float& r2){
+void is_more_then_0(float &r2) {
 
-
-        cout<<"enter radius (more than 0) ";
-        cin>>r2;
-       if (r2<=0)
-       { 
-	cout<<"the data is erroneous";       
-       	       	exit(1);
-	       }
-	
+  cout << "enter radius (more than 0) ";
+  cin >> r2;
+  if (r2 <= 0) {
+    cout << "the data is erroneous";
+    exit(1);
+  }
 }
-
