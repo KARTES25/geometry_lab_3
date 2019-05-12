86d0b91fd05e8910f000740f5bc968c0971eb272
KARTES25 <stilalex@yandex.ru>
2019-05-12 21:30:42 +0700

    create main-test

---
+++ test/main-test.c
@@ -0,0 +1,19 @@
+#include "geometry.h"
+
+int main() {
+  int x1, y1;
+
+  float r1, r2;
+  cout << "Enter parametr of 1st circle(WKT)" << endl;
+  cin >> x1 >> y1;
+
+  is_more_then_0(r1);
+  int x2, y2;
+
+  cout << "Enter parametr of 2nd circle(WKT)" << endl;
+  cin >> x2 >> y2;
+
+  is_more_then_0(r2);
+  circle(x1, y1, r1);
+  circle(x2, y2, r2);
+}
