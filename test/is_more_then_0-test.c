bfb491c4db1a17dd1347d6b78cd55d99bcc1c885
KARTES25 <stilalex@yandex.ru>
2019-05-12 22:28:00 +0700

    add test for function

--- test/is_more_then_0-test.c
+++ test/is_more_then_0-test.c
@@ -3,12 +3,17 @@
 #include <stdlib.h>
 using namespace std;
 
-void is_more_then_0(float &r2) {
+int is_more_then_0(float &r2) {
 
-  cout << "enter radius (more than 0) ";
   cin >> r2;
-  if (r2 <= 0) {
-    cout << "the data is erroneous";
-    exit(1);
-  }
+  if (r2 <= 0) return 0;
+else return 1;
+}
+
+TEST_CASE("Inpur r", "[r]") {
+  REQUIRE(is_more_then_0(-4) == 0);
+  REQUIRE(is_more_then_0(5) == 1);
+  REQUIRE(is_more_then_0(17) == 1);
+  REQUIRE(is_more_then_0(-7) == 0);
+  cout << "Test radius" << endl;
 }
